5fdcae97115ee3063c4a9625fa8ca3ec2e59c5ab
CesarLepeITT <l22212360@tijuana.tecnm.mx>
2025-11-16 00:47:58 -0800

    feat: main interface first working draft

--- ui/main_view.py
+++ ui/main_view.py
@@ -5,11 +5,16 @@ from tkinter import ttk
 from PIL import Image, ImageTk
 import os
 
+
+
+
 class MainView(tk.Tk):
     """
         Main window
     """
-    
+    font_titulos = ("arial", 20, "bold")
+    font_estados = ("arial", 16, "bold")
+
     def __init__(self, controller):
         super().__init__()
         self.title("Catálogo de Fauna Mexicana 3D")
@@ -23,7 +28,7 @@ class MainView(tk.Tk):
         style.configure('TLabel', background='#f7f7f7')
         style.configure("Gray.TFrame", background="#B6B2B2")
 
-        self. create_widgets()
+        self.create_widgets()
 
         
     def create_sidebar():
@@ -38,7 +43,7 @@ class MainView(tk.Tk):
             pady=10
         )
 
-        # Crear Menu Lateral
+        # 1. Crear Menu Lateral
         menu_lateral_frame = ttk.Frame(
             main_frame,
             relief='solid')
@@ -51,7 +56,7 @@ class MainView(tk.Tk):
         ttk.Label(
             menu_lateral_frame,
             text="Animales",
-            font=("arial", 20, "bold")).pack(pady=(0, 10), padx=10
+            font=self.font_titulos).pack(pady=(0, 10), padx=10
         )
 
         # Icono Busqueda
@@ -71,3 +76,81 @@ class MainView(tk.Tk):
         # Input busqueda
         search_entry = ttk.Entry(search_frame)
         search_entry.pack(side=tk.LEFT, fill=tk.X, expand=True)
+        
+        # centrar el label
+        content_frame = ttk.Frame(
+            main_frame,
+            padding=10
+        )
+        content_frame.pack(
+            side=tk.LEFT, 
+            fill=tk.BOTH, 
+            expand=True
+        )
+
+
+        states = self.controller.load_initial_states()
+        for state_name in states:
+            
+            titulo_estado = ttk.Label(
+                content_frame,
+                text=state_name,
+                anchor='center',
+                font=self.font_titulos
+            ).pack(fill='x', padx=10, pady=2)
+
+            animals_row_container = ttk.Frame(
+                        content_frame,
+                        padding=10
+                    )
+            animals_row_container.pack(
+                        fill=tk.BOTH, 
+                        expand=True
+                    )
+
+            cards_wrapper_frame = ttk.Frame(animals_row_container)
+            
+            cards_wrapper_frame.pack() 
+            
+
+            animals = self.controller.load_animals_by_state(state_name)
+            
+            path_img = os.path.join(os.getcwd(), 'ui', 'img', 'search.png')
+            download_icon = tk.PhotoImage(file=path_img)
+
+            for animal in animals:
+                animal_card_frame = ttk.Frame(cards_wrapper_frame) 
+                
+                
+                animal_card_frame.pack(side=tk.LEFT, fill=tk.Y, padx=5, pady=5)
+
+                
+                download_button = ttk.Button(
+                    animal_card_frame, 
+                    image=download_icon
+                )
+                download_button.pack(
+                    ipadx=5,
+                    ipady=5,
+                    expand=True
+                )
+                download_button.image = download_icon 
+
+                nombre_comun = ttk.Label(
+                    animal_card_frame, 
+                    text=animal['nombre_comun']
+                )
+                nombre_comun.pack()
+                
+                nombre_cientifico = ttk.Label(
+                    animal_card_frame, 
+                    text=animal['nombre_cientifico']
+                )
+                nombre_cientifico.pack()
+
+
+
+
+
+
+
